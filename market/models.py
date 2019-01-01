de3243e4da6ca441dcc33fe4564a6b7bd7ae4b77
3liud <ssocratos@gmail.com>
2019-01-02 00:36:56 +0300

    added the market model db strcuture. differnt users and their items added. might encounter some errors on reverse but will be fixed by next commit

--- market/models.py
+++ market/models.py
@@ -5,10 +5,16 @@ from django.urls import reverse
 
 
 class PostSell(models.Model):
-	title = models.CharField(max_length=100)
-	content = models.TextField()
+	title = models.CharField(max_length=50)
+	commodity = models.ImageField(null=True, upload_to='commodity_pics')
+	description = models.TextField(max_length=300, null=True)
 	date_posted = models.DateTimeField(default=timezone.now)
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+	
+	def __str__(self):
+		return str(self) + ":$" + str(self.price)
+		
+	seller = models.ForeignKey(User, on_delete=models.CASCADE)
 	
 	def __str__(self):
 		return self.title
