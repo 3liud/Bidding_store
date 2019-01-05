c8201e9ee3cfbad63acc443897b8e93184988127
3liud <ssocratos@gmail.com>
2019-01-05 21:02:01 +0300

    .

--- market/urls.py
+++ market/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from .views import (
 	PostSellDeleteView,
 	PostSellDetailView,
@@ -7,13 +8,12 @@ from .views import (
 	UserPostSellListView,
 	PostSellCreateView
 )
-from . import views
 
 urlpatterns = [
 	path('', PostSellListView.as_view(), name='market-home'),
 	path('user/<username>/', UserPostSellListView.as_view(), name='user-post'),
 	path('post/<int:pk>/', PostSellDetailView.as_view(), name='postsell-detail'),
-	path('post/new/', PostSellCreateView.as_view(), name='postsell-create'),
+	path('postsell/new/', PostSellCreateView.as_view(), name='postsell-create'),
 	path('post/<int:pk>/update/', PostSellUpdateView.as_view(), name='postsell-update'),
 	path('post/<int:pk>/delete/', PostSellDeleteView.as_view(), name='postsell-delete'),
 	path('about/', views.about, name='market-about'),
