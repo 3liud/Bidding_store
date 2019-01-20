5d46720c35bd68e8e85a2716cea04cb462cbd0f0
3liud <ssocratos@gmail.com>
2019-01-20 08:19:03 +0300

    there was a slight error in the logic in bidding, now adjusted

--- market/models.py
+++ market/models.py
@@ -10,6 +10,16 @@ class PostSell(models.Model):
 	commodity = models.ImageField("Let the buyer see what you are selling, upload a picture",
 	                              null=False, default='', upload_to='commodity_pics')
 	date_posted = models.DateTimeField(default=timezone.now)
+	BID_TIME = (
+		('5 minutes', '5'),
+		('10 minutes', '10'),
+		('20 minutes', '20'),
+		('30 minutes', '30'),
+	)
+	live_time = models.CharField("How long should your item stay in the auction table?", max_length=20, default='5',
+	                             choices=BID_TIME
+	                             )
+	
 	price = models.DecimalField("How much are you selling the item for?", max_digits=10,
 	                            decimal_places=2, default='')
 	seller = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -19,25 +29,19 @@ class PostSell(models.Model):
 	
 	def get_absolute_url(self):
 		return reverse('postsell-detail', kwargs={'pk': self.pk})
-#	bid_list = models.TextField("existing bids")
-	
+# bid_list = models.TextField("existing bids")
+
 
 class Bid(models.Model):
 	bidder = models.ForeignKey(User, on_delete=models.CASCADE)
-
+	
 	def __str__(self):
 		return str(self.bidder)
 	
 	item = models.ForeignKey(PostSell, on_delete=models.CASCADE)
-
+	
 	def get_absolute_url(self):
 		return reverse('postsell-bid', kwargs={'pk': self.pk})
-
+	
 	bid_price = models.DecimalField(max_digits=10, decimal_places=2, default='0.00')
-	BID_TIME = (
-		('5 minutes', '5'),
-		('10 minutes', '10'),
-		('20 minutes', '20'),
-		('30 minutes', '30'),
-			)
-	bid_time = models.CharField(max_length=20, default='5', choices=BID_TIME)
+	bid_time = models.TimeField(default=timezone.now)
